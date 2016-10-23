55cad5f940a4a9c4404cba069b63b06f633a7f3b
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-10-23 13:43:41 +0100

    Interfaz ISemaforo-posiblemente cambie el nombre

--- src/ull/patrones/practica4/Practica4DAP.java
+++ src/ull/patrones/practica4/Practica4DAP.java
@@ -6,7 +6,5 @@ public class Practica4DAP
 {
 	public static void main(String[] args)
 	{
-
 	}
-
 }
---
+++ src/ull/patrones/practica4/estado/ISemaforo.java
@@ -0,0 +1,6 @@
+package ull.patrones.practica4.estado;
+
+public interface ISemaforo
+{
+	public ISemaforo cambiarEstado(ISemaforo a_estado);
+}
